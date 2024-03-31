c341dbced9494f2776e6d6f0876258d805d1eab8
김창균 <ckdrbsaos@naver.com>
2024-03-31 20:56:57 +0900

    회원관리 예제(DI) / 테스트

---
+++ src/main/java/hello/hellospring/Controller/MemberController.java
@@ -0,0 +1,17 @@
+package hello.hellospring.Controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
