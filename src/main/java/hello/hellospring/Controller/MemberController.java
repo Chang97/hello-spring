c097326e67750161613369d52a7d6c8d6ff78af4
김창균 <ckdrbsaos@naver.com>
2024-04-02 21:50:02 +0900

    회원가입 구현

--- src/main/java/hello/hellospring/Controller/MemberController.java
+++ src/main/java/hello/hellospring/Controller/MemberController.java
@@ -1,10 +1,16 @@
 package hello.hellospring.Controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.BeanNameViewResolver;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,5 +19,21 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
